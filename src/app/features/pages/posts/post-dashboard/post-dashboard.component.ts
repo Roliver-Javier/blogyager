4c0b9bc4d14a521228784662aff301b3c8d7725d
RoliverJavier <roliverjavier@gmail.com>
2019-04-30 02:02:53 -0400

    fixing some structure folders

--- src/app/features/pages/posts/post-dashboard/post-dashboard.component.ts
+++ src/app/features/pages/posts/post-dashboard/post-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/core/auth/auth.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 import { PostService } from 'src/app/core/services/post.service';
 
 @Component({
